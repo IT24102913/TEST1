2453899c9ab7d9c9623b252cb5256eb4531e5249
IT24102913 <it24102913@my.sliit.lk>
2026-04-25 13:47:39 +0530

    2nd

---
+++ frontend/src/api.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+// This is the BASE URL of your backend.
+// In the lab, you might need to update this after deployment.
+const API_URL = "https://newready1-production.up.railway.app/api/items";
+// GET all items from the database
+export const getItems = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+// POST a new item to the database
+export const createItem = async (itemData) => {
+  const response = await axios.post(API_URL, itemData);
+  return response.data;
+};
+
+// PUT (Update) an existing item by its unique ID
+export const updateItem = async (id, itemData) => {
+  const response = await axios.put(`${API_URL}/${id}`, itemData);
+  return response.data;
+};
+
+// DELETE an item by its unique ID
+export const deleteItem = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  return response.data;
+};
+
+
+/* const BASE_URL = "https://newready1-production.up.railway.app/api";
+
+const ITEM_URL = `${BASE_URL}/items`;
+const USER_URL = `${BASE_URL}/users`;
+
+// Now use ITEM_URL for item functions and USER_URL for user functions
+*/
